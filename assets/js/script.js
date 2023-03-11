b633e6983b482ea282d682e69d0f81187b458fe6
gennadiy-gaysha <gennadiy.gaysha@gmail.com>
2023-03-11 11:51:44 +0000

    Conflict resolved

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,81 @@
 document.addEventListener('DOMContentLoaded', function () {
-            document.addEventListener('DOMContentLoaded', function () {
-                let buttons = document.getElementsByTagName('button');
+    let buttons = document.getElementsByTagName('button');
 
-                for (let button of buttons) {
-                    button.addEventListener('click', function () {
-                        let attrName = this.getAttribute(
-                            "data-type");
-                        if (attrName === 'submit') {
-                            alert(
-                                `You clicked ${attrName.toUpperCase()} button`
-                                )
-                        } else {
-                            let gameType = attrName;
-                            runGame(gameType);
-                        }
-                    })
-                }
-                runGame('addition');
-            })
-
-
-            /**
-             * The main game "loop" called when the script is first loaded and after the user's answer has been proceed
-             */
-            function runGame(gameType) {
-                num1 = Math.floor(Math.random() * 25 + 1);
-                num2 = Math.floor(Math.random() * 25 + 1);
-
-                if (gameType === 'subtract') {
-                    displaySubtractQuestion(num1, num2);
-                } else if (gameType === 'addition') {
-                    displayAdditionQuestion(num1, num2)
-                } else if (gameType === 'multiply') {
-                    displayMultiplyQuestion(num1, num2)
-                } else if (gameType === 'division') {
-                    displayDivisionQuestion(num1, num2)
-                } else {
-                    alert(`${gameType.toUpperCase()} is undefined`);
-                    throw `${gameType.toUpperCase()} is undefined. Aborted!`
-                }
+    for (let button of buttons) {
+        button.addEventListener('click', function () {
+            let attrName = this.getAttribute(
+                "data-type");
+            if (attrName === 'submit') {
+                alert(
+                    `You clicked ${attrName.toUpperCase()} button`
+                )
+            } else {
+                let gameType = attrName;
+                runGame(gameType);
             }
-            runGame();
+        })
+    }
+    runGame('addition');
+});
+/**
+ * The main game "loop" called when the script is first loaded and after the user's answer has been proceed
+ */
+function runGame(gameType) {
+    num1 = Math.floor(Math.random() * 25 + 1);
+    num2 = Math.floor(Math.random() * 25 + 1);
 
-            function checkAnswer() {
+    if (gameType === 'addition') {
+        displayAdditionQuestion(num1, num2);
+    } else if (gameType === 'subtract') {
+        displaySubtractQuestion(num1, num2)
+    } else if (gameType === 'multiply') {
+        displayMultiplyQuestion(num1, num2)
+    } else if (gameType === 'division') {
+        displayDivisionQuestion(num1, num2)
+    } else {
+        alert(`${gameType.toUpperCase()} is undefined`);
+        throw `${gameType.toUpperCase()} is undefined. Aborted!`
+    }
+}
+runGame();
 
-            }
+function checkAnswer() {
 
-            function calculateCorrectAnswer() {
+}
 
-            }
+function calculateCorrectAnswer() {
 
-            function incrementScore() {
+}
 
-            }
+function incrementScore() {
 
-            function incrementWrongAnswer() {
+}
 
-            }
+function incrementWrongAnswer() {
 
-            function displayAdditionQuestion(operand1, operand2) {
-                document.getElementById('operand1').textContent = operand1;
-                document.getElementById('operand2').textContent = operand2;
-                document.getElementById('operator').textContent = "+";
+}
 
-            }
+function displayAdditionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 
-            function displaySubtractQuestion(operand1, operand2) {
-                document.getElementById('operand1').textContent = operand1;
-                document.getElementById('operand2').textContent = operand2;
-                document.getElementById('operator').textContent = "-";
-            }
+}
 
-            function displayMultiplyQuestion() {
+function displaySubtractQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "-";
+}
 
-            }
+function displayMultiplyQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "*";
+}
 
-            function displayDivisionQuestion() {
-
-            }
+function displayDivisionQuestion(operand1, operand2) {
+    document.getElementById('operand1').textContent = operand1;
+    document.getElementById('operand2').textContent = operand2;
+    document.getElementById('operator').textContent = "/";
+}
