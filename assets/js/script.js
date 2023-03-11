441eee330b3b1ef5c4e89c0dbe527ffa505bad4b
gennadiy-gaysha <gennadiy.gaysha@gmail.com>
2023-03-11 09:14:55 +0000

    Add eventListeners for document loading and button clicks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function(){
+            let attrName = this.getAttribute("data-type");
+            if(attrName === 'submit'){
+                alert(`You clicked ${attrName.toUpperCase()} button`)
+            } else {
+                alert(`You chose ${attrName.toUpperCase()} game`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -27,5 +42,8 @@ function displaySubtractQuestion() {
 }
 
 function displayMultiplyQuestion() {
+    
+}
+function displayDivisionQuestion() {
     
 }
